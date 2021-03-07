cd8238da6f11072ae3b19f4ac1229e9fe6d57df6
parthasarath1 <72913228+parthasarath1@users.noreply.github.com>
2021-03-07 12:44:26 +0530

    📝Added Contrubuting.md, config.json for easy dev,.env_exmaple (#7)
    * Added Contrubuting.md, config.json for easy dev,.env_exmaple for contrubution
    
    * Update .env_example
    
    * Update Contributing.md
    
    * Delete config.json
    
    * Update onMessage.js
    
    Co-authored-by: Megatank58 <51410502+MEGATANK58@users.noreply.github.com>

---
+++ Contributing.md
@@ -0,0 +1,33 @@
+## Contrubuting
+
+1. Fork the project
+
+2. Clone your forked project and get into project directory
+
+   ```bash
+   git clone https://github.com/parthasarath1/Crypton
+   cd Crypton
+   ```
+
+3. Install All dependncies
+
+```bash
+npm install
+
+```
+
+5. Rename `.env_example` to `.env` and add your credentials for Development
+
+6. Add your Desired changes/features.
+
+7. commit your changes by
+
+```bash
+   git add.
+   git commit -m "your message"
+```
+
+8. Compare and send Pull request to us
+
+**Note:** While you make Pull request make sure you List all the things you have added
+and changes you have made to the project.
--- index.js
+++ index.js
@@ -7,33 +7,39 @@ const mongoose = require("mongoose");
 /* The client object */
 const client = new discord.Client();
 /* Mongo DB connection */
-mongoose.connect(process.env.mongodbtoken, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 /* Commands and categories */
 client.commands = new discord.Collection();
 client.categories = [];
 /* Go through each folder and command and add them to the collection */
 const folders = fs.readdirSync("./commands");
 for (const folder of folders) {
-	client.categories.push(folder.toLowerCase());
-	const commands = fs.readdirSync(`./commands/${folder}`);
-	for (const commandFile of commands) {
-		const command = require(`./commands/${folder}/${commandFile}`);
-		client.commands.set(command.name.toLowerCase(), command);
-	}
+  client.categories.push(folder.toLowerCase());
+  const commands = fs.readdirSync(`./commands/${folder}`);
+  for (const commandFile of commands) {
+    const command = require(`./commands/${folder}/${commandFile}`);
+    client.commands.set(command.name.toLowerCase(), command);
+  }
 }
 /* Go through each event file and add them */
-const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"));
 for (const eventFile of eventFiles) {
-	const event = require(`./events/${eventFile}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.run(...args, client));
-	} else {
-		client.on(event.name, (...args) => event.run(...args, client));
-	}
+  const event = require(`./events/${eventFile}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.run(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.run(...args, client));
+  }
 }
 /* Handle errors */
-process.on("unhandledRejection", err => {
-	console.log(err);
+process.on("unhandledRejection", (err) => {
+  console.log(err);
 });
 /* Log-in the client using super secret token */
-client.login(process.env.token);
+client.login(process.env.TOKEN);
