2061f2ac7df20316bf8d784fe45183ca1a5f8f89
Megatank58 <51410502+MEGATANK58@users.noreply.github.com>
2021-03-14 12:49:35 +0530

    Update onceReady.js (#14)
    * Update onceReady.js

--- events/onceReady.js
+++ events/onceReady.js
@@ -3,5 +3,7 @@ module.exports = {
 	once: true,
 	run: (client) => {
 		console.log(`Logged in as ${client.user.tag}!`);
+		client.user.setActivity(`:Help in ${client.guilds.cache.size} guilds with ${client.users.cache.size} users!`,
+		{ type: "PLAYING", } );
 	},
 };
