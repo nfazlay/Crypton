abd4cadef50818ea6114e048fd1ce9509f200f8b
Megatank58 <tanumegatank@gmail.com>
2021-03-24 09:49:24 +0530

    ESlint error fixed

--- index.js
+++ index.js
@@ -98,5 +98,5 @@ process.on("uncaughtException", (error) => console.log(error));
 if (process.env.TOKEN) {
   client.login(process.env.TOKEN);
 } else {
-  console.error();("ERROR: Token not found in .env file! Make sure you have the token correct")
+  console.error();("ERROR: Token not found in .env file! Make sure you have the token correct");
 }
