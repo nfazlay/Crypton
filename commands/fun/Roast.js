02776ee5143ccef672a3db15c6c89f5da2b3393f
Loosy_ Goosy <65233336+vedadub@users.noreply.github.com>
2021-04-02 13:35:20 +0530

    New Roast lines added
    This is my first commit, I've added two new roast lines for the command!
    
    Co-Authored-By: Megatank58 <51410502+MEGATANK58@users.noreply.github.com>

--- commands/fun/Roast.js
+++ commands/fun/Roast.js
@@ -4,7 +4,7 @@ module.exports =
 	description: "haha roasted",
 	cooldown: 2,
 	run: async(message) => {
-		const answers = ["Sup normie?", "Hey idiot", "whats up noob", "Did i ask?", "I dont care", "Another idiot", "The king of loosers", "BOOMER", "Novice", "Normie be like", "Sup edot"];
+		const answers = ["Sup normie?", "Hey idiot", "whats up noob", "Did i ask?", "I dont care", "Another idiot", "The king of loosers", "BOOMER", "Novice", "Normie be like", "Sup edot", "Looks like there a noob in the room.", "Roast yourself, I'm tired"];
 		const answer = answers[Math.floor(Math.random() * answers.length)];
 		message.channel.send(answer);
 	}
