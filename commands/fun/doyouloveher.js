56f00576f32c956a38918e47371cce4f5ca89867
Megatank58 <51410502+MEGATANK58@users.noreply.github.com>
2021-05-19 19:24:00 +0530

    Bug Fix (#43)
    Bug fix for loverate command.
    
    Co-authored-by: Loosy_ Goosy <65233336+vedadub@users.noreply.github.com>

--- commands/fun/doyouloveher.js
+++ commands/fun/doyouloveher.js
@@ -4,6 +4,9 @@ module.exports = {
 	cooldown: 2,
 	run: async (message) => {
 		const men = message.mentions.users.first();
+		if (!men) {
+			return message.channel.send("Please mention the person");
+		}
 		const answer = Math.floor(Math.random() * 100);
 		message.channel.send(
 			`${message.author.username}'s Love towards ${men} is ${answer}% true!`
